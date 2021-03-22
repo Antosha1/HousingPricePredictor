d3eda4b38cd5d9b4d5c74eda9ad87b961f3bf0da
Antosha1 <sotnikov.ad@phystech.edu>
2021-03-23 00:05:28 +0300

    add docstring in mylib/utils.py

--- mylib/utils.py
+++ mylib/utils.py
@@ -3,5 +3,11 @@ import seaborn as sns
 
 
 def read_data(file_path):
+    """
+    Read data from .csv file
+
+    :param file_path: a path to data
+    :return: pandas-like dataframe contains data
+    """
     dataframe = pd.read_csv(file_path, sep='\t', index_col=0)
     return dataframe
